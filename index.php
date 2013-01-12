0abecb72d9a4d99a347ff3c90904915bdfa94018
Shubham <smsmittal14@gmail.com>
2013-01-12 07:31:23 +0530

    Home Page file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="#">HOME</a><br/>
+        <a href="forms/Testskill.php">Test Your Skills</a><br/>
+        <a href="forms/UploadQuestion.php">Upload Question</a><br/>
+        <a href="forms/LoginPage.php">Login</a><br/>
+        <a href="forms/Register.php">Register</a><br/>
+    </body>
+</html>
